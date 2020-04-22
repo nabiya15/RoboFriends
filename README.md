6130c467bde07fbf60024d0fc2c9e7a3893ff30d
unknown <m.nabiya@gmail.com>
2020-04-22 16:09:56 -0500

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# RoboFriends
-
-#### Meet my robot friends in 3 easy steps :)
- 1. Clone the repo
- 1. npm install
- 1. npm start
---
+++ precache-manifest.e9f27c1f462dbc6e92197255e03bb004.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193e7e128f85f3f3badd2f28c1c65149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "364a75b09cd8f0382eb0",
+    "url": "/static/css/main.81213842.chunk.css"
+  },
+  {
+    "revision": "a76249b7330f2fa9434c",
+    "url": "/static/js/2.2f7a48c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f7a48c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "364a75b09cd8f0382eb0",
+    "url": "/static/js/main.e2795b8e.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
